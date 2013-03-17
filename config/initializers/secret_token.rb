8e9c43398a1dd0561348ec016842d3e07a44f226
Andrew <andrewphillipheil@facebook.com>
2013-03-16 23:09:46 -0400

    rails generate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialtrip::Application.config.secret_token = '8d5b6acb69145d8ce579bd57d68fc8d6cd6721e0acd1f83343569638e289c7d0364c525fa7a4cdd3de57c2af040292c09b6d5fe3ede74fa953b418edc4a6b4eb'
