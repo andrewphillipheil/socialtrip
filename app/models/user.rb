a465f5d77199a3da50439464481b1cd18df994f7
code-m <mayank@mayank-Compaq-Presario-C700-Notebook-PC.(none)>
2013-03-21 01:19:17 +0530

    [#46412195] added omniauth controller,generated resource for user

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+   unless user
+     user = User.create(name:auth.extra.raw_info.name,
+                          provider:auth.provider,
+                          uid:auth.uid,
+                          email:auth.info.email,
+                          first_name: auth.extra.raw_info.first_name,
+        				  last_name: auth.extra.raw_info.last_name,
+                          password:Devise.friendly_token[0,20]
+                          )
+   end
+  	user
+	end
+end
