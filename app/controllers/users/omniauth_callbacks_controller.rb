48f08a5cc5649fc6baacb7f48833b1035b6d8ecb
code-m <mayank@mayank-Compaq-Presario-C700-Notebook-PC.(none)>
2013-03-22 00:27:56 +0530

    [#46412195] added code for facebook authentication and adding user details

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,5 +4,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?    
-    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,12 @@ class User < ActiveRecord::Base
   devise :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :first_name, :last_name
   # attr_accessible :title, :body
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
   user = User.where(:provider => auth.provider, :uid => auth.uid).first
    unless user
-     user = User.create(name:auth.extra.raw_info.name,
-                          provider:auth.provider,
+     user = User.create( provider:auth.provider,
                           uid:auth.uid,
                           email:auth.info.email,
                           first_name: auth.extra.raw_info.first_name,
