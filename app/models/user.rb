8399e9d65e7b28cf15227084545a6558355cebcc
w3villa <w3villa@w3villa-Inspiron-3521.(none)>
2013-04-07 22:52:41 +0530

    create associated provider in user create callback and updated ind_for_facebook_oauth accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,30 @@ class User < ActiveRecord::Base
 
   devise :omniauthable, :omniauth_providers => [:facebook]
 
+  attr_accessor :auth_hash
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :remember_me, :provider, :uid, :first_name, :last_name
+  attr_accessible :email, :password, :remember_me, :provider, :uid, :first_name, :last_name, :auth_hash
+
+  after_create :associate_provider
   # attr_accessible :title, :body
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    user = Provider.where(:user_provider => auth.provider, :uid => auth.uid).first.try(:user)
     unless user
-     user = User.create( provider:auth.provider,
-                          uid:auth.uid,
-                          email:auth.info.email,
-                          first_name: auth.extra.raw_info.first_name,
-        				          last_name: auth.extra.raw_info.last_name,
-                          password:Devise.friendly_token[0,20]
-                          )
+     user = User.create(email:auth.info.email,
+                        first_name: auth.extra.raw_info.first_name,
+        				        last_name: auth.extra.raw_info.last_name,
+                        password:Devise.friendly_token[0,20],
+                        auth_hash: auth
+                        )
     end
     user
 	end
+
+  private
+
+    def associate_provider
+      provider = providers.build(:user_provider => auth_hash.provider, :uid => auth_hash.uid)
+      provider.save
+    end
 end
