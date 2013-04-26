71d0b711e2f122d16e594e36316c04248ac35bbe
Preeti Gupta <guptapreeti.india@gmail.com>
2013-04-27 03:20:24 +0530

    updated email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,13 @@ Socialtrip::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => '50.112.18.112' }
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => false,
+    :openssl_verify_mode => 'none'
+  }
+
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
